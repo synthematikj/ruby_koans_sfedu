a4780ab884d8b452e2cad1b2a83327d9c043bfad
Maria <mvputrova@gmail.com>
2024-03-04 22:48:41 +0300

    Updated

--- koans/about_nil.rb
+++ koans/about_nil.rb
@@ -5,9 +5,7 @@ class AboutNil < Neo::Koan
     assert_equal true, nil.is_a?(Object), "Unlike NULL in other languages"
   end
 
-  #todo
-  """
-    def test_you_dont_get_null_pointer_errors_when_calling_methods_on_nil
+  def test_you_dont_get_null_pointer_errors_when_calling_methods_on_nil
     # What happens when you call a method that doesn't exist.  The
     # following begin/rescue/end code block captures the exception and
     # makes some assertions about it.
@@ -15,14 +13,14 @@ class AboutNil < Neo::Koan
       nil.some_method_nil_doesnt_know_about
     rescue Exception => ex
       # What exception has been caught?
-      assert_equal NoMethodError, ex.class
+      assert_equal __(NoMethodError), ex.class
 
       # What message was attached to the exception?
       # (HINT: replace __ with part of the error message.)
-      assert_match(NoMethodError, ex.message)
+      assert_match(/#{__("undefined method")}/, ex.message)
     end
   end
-  """
+
 
   def test_nil_has_a_few_methods_defined_on_it
     assert_equal true, nil.nil?
